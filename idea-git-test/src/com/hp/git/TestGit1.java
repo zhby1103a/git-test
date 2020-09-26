404286f557f49223750a2ad01edc776402a1236b
zhby1103 <183158080@qq.com>
2020-09-26 19:14:00 +0800

    把idea-git-test工程添加到git

---
+++ idea-git-test/src/com/hp/git/TestGit1.java
@@ -0,0 +1,10 @@
+package com.hp.git;
+
+public class TestGit1 {
+
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
